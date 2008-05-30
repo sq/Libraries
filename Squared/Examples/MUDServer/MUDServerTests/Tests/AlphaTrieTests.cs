234619d5359b6f64cc13305e4fa3b1b247b9dca4
kevin.gadd <kevin.gadd@1953d6fe-3836-0410-a91e-07494a8b0067>
2008-05-30 17:57:42 +0000

    Add more tests
    git-svn-id: http://fracture.googlecode.com/svn/trunk@97 1953d6fe-3836-0410-a91e-07494a8b0067

--- Squared/Examples/MUDServer/MUDServerTests/Tests/AlphaTrieTests.cs
+++ Squared/Examples/MUDServer/MUDServerTests/Tests/AlphaTrieTests.cs
@@ -13,6 +13,49 @@ namespace MUDServer {
         }
 
         [Test]
+        public void MultipleInsertion () {
+            var AT = new AlphaTrie<string>();
+            AT.Insert("first", "word");
+            Assert.IsNotNull(AT.FindByKeyExact("first"));
+            AT.Insert("firsts", "awesome");
+            Assert.IsNotNull(AT.FindByKeyExact("first"));
+            Assert.IsNotNull(AT.FindByKeyExact("firsts"));
+            AT.Insert("second", "time");
+            Assert.IsNotNull(AT.FindByKeyExact("first"));
+            Assert.IsNotNull(AT.FindByKeyExact("firsts"));
+            Assert.IsNotNull(AT.FindByKeyExact("second"));
+            AT.Insert("seconds", "matter");
+            Assert.IsNotNull(AT.FindByKeyExact("first"));
+            Assert.IsNotNull(AT.FindByKeyExact("firsts"));
+            Assert.IsNotNull(AT.FindByKeyExact("second"));
+            Assert.IsNotNull(AT.FindByKeyExact("seconds"));
+            AT.Insert("secondary", "place");
+            Assert.IsNotNull(AT.FindByKeyExact("first"));
+            Assert.IsNotNull(AT.FindByKeyExact("firsts"));
+            Assert.IsNotNull(AT.FindByKeyExact("second"));
+            Assert.IsNotNull(AT.FindByKeyExact("seconds"));
+            Assert.IsNotNull(AT.FindByKeyExact("secondary"));
+        }
+
+        [Test]
+        public void RemoveTest () {
+            var AT = new AlphaTrie<string>();
+            AT.Insert("first", "word");
+            AT.Insert("firsts", "awesome");
+            AT.Insert("second", "time");
+            AT.Insert("seconds", "matter");
+            AT.Insert("secondary", "place");
+
+            Assert.IsTrue(AT.Remove("first"));
+            Assert.IsTrue(AT.Remove("seconds"));
+            Assert.IsFalse(AT.Remove("seconds"));
+            Assert.IsFalse(AT.Remove("pancakes"));
+            Assert.IsNotNull(AT.FindByKeyExact("firsts"));
+            Assert.IsNotNull(AT.FindByKeyExact("second"));
+            Assert.IsNotNull(AT.FindByKeyExact("secondary"));
+        }
+
+        [Test]
         public void NoNullValues () {
             var AT = new AlphaTrie<string>();
             try {
