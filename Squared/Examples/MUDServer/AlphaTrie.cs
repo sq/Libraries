8dc6a0182ffc5eee37fa96ce9ede5c887cdd667f
sekkusu <sekkusu@1953d6fe-3836-0410-a91e-07494a8b0067>
2008-05-30 08:20:36 +0000

    Added a new AlphaTrie class that maps alphabetical strings (case-insensitive) to a type T for that instance. Lookup is at most O(k), where k is the longest length string in the Trie. Insertion is also at most O(k).
    AlphaTrie is used to find a certain string or find the best completion for a string (such as 'loo'->'look')
    
    AlphaTrie still needs a Remove function to be implemented before it can be used for the next logical step (explained below.)
    
    Changed the Player class to use an AlphaTrie to keep track of the commands available to that player. Even though it only takes microseconds in order to construct their personal trie, as the number of commands increases, it'd probably be wise to keep a static instance of a base starting command list and create a copy constructor for AlphaTrie, then when a player logs on, copying that AlphaTrie over to their personal instance.
    
    Changed the commands to map command names to delegates, and implemented the commands as such. Now process input checks the trie for an appropriate command and if one is found that delegate is called.
    
    The next step, after Remove is added (which won't take much) is to set it so that exit names in a room are alphabetical only, and when you enter a room, the room automatically adds those exit names to your commands list. Then, if there is an exit named 'north' in the room, for example, you'll automatically have a 'north' command to use that maps to it. Go <x> will still be used for Go'ing to an exit based on the description. When you exit a room, it'll appropriately remove the old exit commands.
    
    Additionally, this means that each room or even each entity in a room can have their own commands they grant the players in the room (for example a room that has a level can have a 'pull' command coded specifically for that room.)
    
    Finally, it can be used for qualifying the player for special commands like Guild commands or Admin commands, where only if they have those flags will those commands be added to their repertoire.
    
    AlphaTrie doesn't do any explicit Disposal, but I don't think that the GC would have a difficult time keeping up with the references if an AlphaTrie goes out of scope.
    
    I'd also like to start a MUDServer test suite, especially for AlphaTrie testing. Might do that this weekend.
    
    Some other ideas for AlphaTrie include using it for the connected players list, so that if you do 'tell sekk hi' it will map sekk-> Sekkusu if that player is connected.
    
    git-svn-id: http://fracture.googlecode.com/svn/trunk@89 1953d6fe-3836-0410-a91e-07494a8b0067

---
+++ Squared/Examples/MUDServer/AlphaTrie.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace MUDServer {
+    public interface IAlphaTrie<T> where T : class {
+        // Searches the trie for a node with the exact key provided and returns the value of that node.
+        KeyValueReference<string, T> FindByKeyExact (string key);
+        // Searches the trie for a node starting with the given key. If multiple are found, null will be returned.
+        KeyValueReference<string, T> FindByKeyStart (string key);
+        // Inserts a new key-value pair into the trie.
+        void Insert (string key, T value);
+        // Removes a key-value pair from the trie by its key.
+        void Remove (string key);
+        // Traverse the trie with an enumerator.
+        IEnumerable<KeyValueReference<string, T>> Traverse ();
+    }
+
+    public class AlphaTrie<T> : IAlphaTrie<T> where T : class {
+
+        AlphaTrieNode rootNode;
+
+        private class AlphaTrieNode {
+            public AlphaTrieNode[] Nodes = null;
+            public KeyValueReference<string, T> Value = null;
+            public int Branches = 0;
+
+            public AlphaTrieNode () {
+            }
+
+            public AlphaTrieNode (string _key, T _value) {
+                Value = new KeyValueReference<string, T>(_key, _value);
+            }
+
+            public AlphaTrieNode (KeyValueReference<string, T> KVR) {
+                Value = KVR;
+            }
+
+            public static int CharToTrieNodeIndex (char C) {
+                if (C < 'a' || C > 'z')
+                    throw new InvalidOperationException("Attempted to convert an invalid character to a trie index.");
+                else
+                    return (C - 'a');
+            }
+        }
+
+        public AlphaTrie () {
+            rootNode = new AlphaTrieNode();
+        }
+
+        public KeyValueReference<string, T> FindByKeyExact (string key) {
+            KeyValueReference<string, T> KVRTemp = FindByKeyStart(key);
+            if (KVRTemp == null || KVRTemp.Key != key.ToLower())
+                return null;
+            else
+                return KVRTemp;
+
+        }
+
+        public KeyValueReference<string, T> FindByKeyStart (string _key) {
+            string key = _key.ToLower();
+            AlphaTrieNode currentNode = rootNode;
+
+            int level = -1;
+            while (level < key.Length - 1) {
+                level++;
+
+                // Found the key exactly here.
+                if (currentNode.Value != null && currentNode.Value.Key == key)
+                    return currentNode.Value;
+                
+                int ix = AlphaTrieNode.CharToTrieNodeIndex(key[level]);
+
+                // Ran into a dead end.
+                if (currentNode.Nodes == null || currentNode.Nodes[ix] == null) {
+                    if (currentNode.Value == null)
+                        return null;
+                    if (currentNode.Value.Key.StartsWith(key))
+                        return currentNode.Value;
+                    return null;
+                }
+
+                currentNode = currentNode.Nodes[ix];
+            }
+
+            while (true) {
+                if (currentNode.Branches > 1)
+                    return null;
+
+                if (currentNode.Value != null && currentNode.Branches == 0)
+                    return currentNode.Value;
+
+                // Indeterminate. There's one value here, but there's another longer value later on.
+                if (currentNode.Value != null && currentNode.Branches == 1)
+                    return null;
+
+                // Guaranteed 1 branch.
+                for (int i = 0; i < 26; i++) {
+                    if (currentNode.Nodes[i] != null) {
+                        currentNode = currentNode.Nodes[i];
+                        break;
+                    }
+                }
+            }
+
+        }
+
+        public void Insert (string key, T value) {
+            if (key.Length == 0)
+                throw new InvalidOperationException("Attempted to insert a blank key into the trie.");
+
+            Insert(rootNode, new KeyValueReference<string, T>(key.ToLower(), value), 0);
+        }
+
+        private void Insert (AlphaTrieNode node, KeyValueReference<string, T> insKV, int level) {
+
+            if (node.Value != null && node.Value.Key == insKV.Key)
+                throw new InvalidOperationException("Attempted to insert duplicate key into the trie.");
+
+            if (node.Nodes == null && node.Value == null) {
+                node.Value = insKV;
+                return;
+            }
+
+            if (node.Nodes == null && node.Value != null) {
+                node.Nodes = new AlphaTrieNode[26];
+                // Only move the current Value if it isn't exactly there.
+                if (node.Value.Key.Length > level) {
+                    KeyValueReference<string, T> moveKV = node.Value;
+                    node.Value = null;
+                    int im = AlphaTrieNode.CharToTrieNodeIndex(moveKV.Key[level]);
+                    node.Nodes[im] = new AlphaTrieNode(moveKV);
+                    node.Branches++;
+                }
+                // If ours fits perfectly, we know we had to move the old one.
+                if (insKV.Key.Length == level) {
+                    node.Value = insKV;
+                    return;
+                }
+
+                int ii = AlphaTrieNode.CharToTrieNodeIndex(insKV.Key[level]);
+                if (node.Nodes[ii] == null) {
+                    node.Nodes[ii] = new AlphaTrieNode(insKV);
+                    node.Branches++;
+                }
+                else
+                    Insert(node.Nodes[ii], insKV, level + 1);
+                return;
+            }
+
+            if (node.Nodes != null) {
+                // If we go exactly here, stick it in the Value.
+                if (insKV.Key.Length == level) {
+                    node.Value = insKV;
+                    return;
+                }
+                // Otherwise, make new path or follow the old path.
+                int ii = AlphaTrieNode.CharToTrieNodeIndex(insKV.Key[level]);
+                if (node.Nodes[ii] == null) {
+                    node.Nodes[ii] = new AlphaTrieNode(insKV);
+                    node.Branches++;
+                }
+                else
+                    Insert(node.Nodes[ii], insKV, level + 1);
+            }
+
+        }
+
+        public void Remove (string key) {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<KeyValueReference<string, T>> Traverse () {
+            return Traverse(rootNode);
+        }
+
+        private IEnumerable<KeyValueReference<string, T>> Traverse (AlphaTrieNode startingNode) {
+            if (startingNode.Value != null)
+                yield return startingNode.Value;
+
+            if (startingNode.Nodes != null) {
+                for (int i = 0; i < 26; i++) {
+                    AlphaTrieNode nextNode = startingNode.Nodes[i];
+                    if (nextNode != null)
+                        foreach (var newNode in this.Traverse(nextNode))
+                            yield return newNode;
+                }
+            }
+        }
+
+    }
+
+    public class KeyValueReference<K, V> {
+        K _Key;
+        V _Value;
+
+        public K Key { get { return _Key; } }
+        public V Value { get { return _Value; } }
+
+        public KeyValueReference (K __Key, V __Value) {
+            _Key = __Key;
+            _Value = __Value;
+        }
+    }
+}
--- Squared/Examples/MUDServer/Player.cs
+++ Squared/Examples/MUDServer/Player.cs
@@ -9,11 +9,16 @@ namespace MUDServer {
         public TelnetClient Client;
         private bool _LastPrompt;
         private int _NumMessagesSent;
+        private delegate IEnumerator<object> CommandHandler (Player p, string[] words);
+        private IAlphaTrie<CommandHandler> _Commands;
 
         public Player (TelnetClient client, Location location)
             : base(location, null) {
             Client = client;
             client.RegisterOnDispose(OnDisconnected);
+
+            _Commands = new AlphaTrie<CommandHandler>();
+            AddPlayerCommands(_Commands);
 
             AddEventHandler(EventType.CombatHit, OnEventCombatHit);
             AddEventHandler(EventType.CombatMiss, OnEventCombatMiss);
@@ -69,6 +74,164 @@ namespace MUDServer {
             }
         }
 
+        private static void AddPlayerCommands(IAlphaTrie<CommandHandler> _) {
+            _.Insert(
+                "say",
+                delegate(Player p, string[] words) {
+                    if (words.Length < 2) {
+                        p.SendMessage("What did you want to <say>, exactly?");
+                        p.SendPrompt();
+                    }
+                    else {
+                        Event.Send(new { Type = EventType.Say, Sender = p, Text = string.Join(" ", words, 1, words.Length - 1) });
+                    }
+                    return null;
+                });
+
+            _.Insert(
+                "emote",
+                delegate(Player p, string[] words) {
+                    if (words.Length < 2) {
+                        p.SendMessage("What were you trying to do?");
+                        p.SendPrompt();
+                    }
+                    else {
+                        Event.Send(new { Type = EventType.Emote, Sender = p, Text = string.Join(" ", words, 1, words.Length - 1) });
+                    }
+                    return null;
+                });
+
+            _.Insert(
+                "tell",
+                delegate(Player p, string[] words) {
+                    if (words.Length < 3) {
+                        p.SendMessage("Who did you want to <tell> what?");
+                        p.SendPrompt();
+                    }
+                    else {
+                        string name = words[1];
+                        IEntity to = p.Location.ResolveName(name);
+                        if (to != null) {
+                            Event.Send(new { Type = EventType.Tell, Sender = p, Recipient = to, Text = string.Join(" ", words, 2, words.Length - 2) });
+                        }
+                        else {
+                            p.SendMessage("Who do you think you're talking to? There's nobody named {0} here.", name);
+                            p.SendPrompt();
+                        }
+                    }
+                    return null;
+                });
+
+            _.Insert(
+                "look",
+                delegate(Player p, string[] words) {
+                    p.PerformLook();
+                    p.SendPrompt();
+                    return null;
+                });
+
+            _.Insert(
+                "go",
+                delegate(Player p, string[] words) {
+                    if (p.CurrentHealth <= 0) {
+                        p.SendMessage("You can't do that while dead.");
+                        p.SendPrompt();
+                        return null;
+                    }
+                    try {
+                        int exitId;
+                        string exitText = string.Join(" ", words, 1, words.Length - 1).Trim().ToLower();
+                        Action<Exit> go = (exit) => {
+                            if (World.Locations.ContainsKey(exit.Target.ToLower()))
+                                p.Location = World.Locations[exit.Target.ToLower()];
+                            else {
+                                Console.WriteLine("Warning: '{0}' exit '{1}' leads to undefined location '{2}'.", p.Location.Name, exit.Description, exit.Target);
+                                p.SendMessage("Your attempt to leave via {0} is thwarted by a mysterious force.", exit.Description);
+                                p.SendPrompt();
+                            }
+                        };
+                        if (int.TryParse(exitText, out exitId)) {
+                            go(p.Location.Exits[exitId]);
+                        }
+                        else {
+                            foreach (var e in p.Location.Exits) {
+                                if (e.Description.ToLower().Contains(exitText)) {
+                                    go(e);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    catch {
+                        p.SendMessage("You can't find that exit.");
+                        p.SendPrompt();
+                    }
+                    return null;
+                });
+
+            _.Insert(
+                "kill",
+                delegate(Player p, string[] words) {
+                    if (words.Length < 2) {
+                        p.SendMessage("Who did you want to kill?");
+                        p.SendPrompt();
+                    }
+                    else if (p.CurrentHealth <= 0) {
+                        p.SendMessage("You can't do that while dead.");
+                        p.SendPrompt();
+                    }
+                    else if (p.InCombat) {
+                        p.SendMessage("You're already busy fighting!");
+                        p.SendPrompt();
+                    }
+                    else {
+                        string name = words[1];
+                        IEntity to = p.Location.ResolveName(name);
+                        if (to == p) {
+                            p.SendMessage("You don't really want to kill yourself, you're just looking for attention.");
+                            p.SendPrompt();
+                        }
+                        else if (to != null) {
+                            if (to is CombatEntity) {
+                                CombatEntity cto = to as CombatEntity;
+                                if (cto.InCombat == false) {
+                                    p.StartCombat(cto);
+                                    cto.StartCombat(p);
+                                    Event.Send(new { Type = EventType.CombatStart, Sender = p, Target = cto });
+                                }
+                                else {
+                                    p.SendMessage("They're already in combat, and you don't want to interfere.");
+                                    p.SendPrompt();
+                                }
+                            }
+                            else {
+                                p.SendMessage("You don't think that's such a great idea.");
+                                p.SendPrompt();
+                            }
+                        }
+                        else {
+                            p.SendMessage("Who are you trying to kill, exactly? There's nobody named {0} here.", name);
+                            p.SendPrompt();
+                        }
+                    }
+                    return null;
+                });
+
+            _.Insert(
+                "help",
+                delegate(Player p, string[] words) {
+                    p.SendMessage("You can <say> things to those nearby, if you feel like chatting.");
+                    p.SendMessage("You can also <tell> somebody things if you wish to speak privately.");
+                    p.SendMessage("You can also <emote> within sight of others.");
+                    p.SendMessage("If you're feeling lost, try taking a <look> around.");
+                    p.SendMessage("If you wish to <go> out an exit, simply speak its name or number.");
+                    p.SendMessage("Looking to make trouble? Try to <kill> someone!");
+                    p.SendPrompt();
+                    return null;
+                });
+
+        }
+
         public IEnumerator<object> ProcessInput (string text) {
             string[] words = text.Split(' ');
             if (words.Length < 1)
@@ -77,126 +240,13 @@ namespace MUDServer {
 
             _LastPrompt = false;
 
-            switch (firstWord) {
-                case "say":
-                    if (words.Length < 2) {
-                        SendMessage("What did you want to <say>, exactly?");
-                        SendPrompt();
-                    } else {
-                        Event.Send(new { Type = EventType.Say, Sender = this, Text = string.Join(" ", words, 1, words.Length - 1) });
-                    }
-                    return null;
-                case "emote":
-                    if (words.Length < 2) {
-                        SendMessage("What were you trying to do?");
-                        SendPrompt();
-                    } else {
-                        Event.Send(new { Type = EventType.Emote, Sender = this, Text = string.Join(" ", words, 1, words.Length - 1) });
-                    }
-                    return null;
-                case "tell":
-                    if (words.Length < 3) {
-                        SendMessage("Who did you want to <tell> what?");
-                        SendPrompt();
-                    } else {
-                        string name = words[1];
-                        IEntity to = Location.ResolveName(name);
-                        if (to != null) {
-                            Event.Send(new { Type = EventType.Tell, Sender = this, Recipient = to, Text = string.Join(" ", words, 2, words.Length - 2) });
-                        } else {
-                            SendMessage("Who do you think you're talking to? There's nobody named {0} here.", name);
-                            SendPrompt();
-                        }
-                    }
-                    return null;
-                case "look":
-                    PerformLook();
-                    SendPrompt();
-                    return null;
-                case "go":
-                    if (CurrentHealth <= 0) {
-                        SendMessage("You can't do that while dead.");
-                        SendPrompt();
-                        return null;
-                    }
-                    try {
-                        int exitId;
-                        string exitText = string.Join(" ", words, 1, words.Length - 1).Trim().ToLower();
-                        Action<Exit> go = (exit) => {
-                            if (World.Locations.ContainsKey(exit.Target.ToLower()))
-                                Location = World.Locations[exit.Target.ToLower()];
-                            else {
-                                Console.WriteLine("Warning: '{0}' exit '{1}' leads to undefined location '{2}'.", Location.Name, exit.Description, exit.Target);
-                                SendMessage("Your attempt to leave via {0} is thwarted by a mysterious force.", exit.Description);
-                                SendPrompt();
-                            }
-                        };
-                        if (int.TryParse(exitText, out exitId)) {
-                            go(Location.Exits[exitId]);
-                        } else {
-                            foreach (var e in Location.Exits) {
-                                if (e.Description.ToLower().Contains(exitText)) {
-                                    go(e);
-                                    break;
-                                }
-                            }
-                        }
-                    } catch {
-                        SendMessage("You can't find that exit.");
-                        SendPrompt();
-                    }
-                    return null;
-                case "kill":
-                    if (words.Length < 2) {
-                        SendMessage("Who did you want to kill?");
-                        SendPrompt();
-                    } else if (CurrentHealth <= 0) {
-                        SendMessage("You can't do that while dead.");
-                        SendPrompt();
-                    } else if (InCombat) {
-                        SendMessage("You're already busy fighting!");
-                        SendPrompt();
-                    } else {
-                        string name = words[1];
-                        IEntity to = Location.ResolveName(name);
-                        if (to == this) {
-                            SendMessage("You don't really want to kill yourself, you're just looking for attention.");
-                            SendPrompt();
-                        } else if (to != null) {
-                            if (to is CombatEntity) {
-                                CombatEntity cto = to as CombatEntity;
-                                if (cto.InCombat == false) {
-                                    this.StartCombat(cto);
-                                    cto.StartCombat(this);
-                                    Event.Send(new { Type = EventType.CombatStart, Sender = this, Target = cto });
-                                } else {
-                                    SendMessage("They're already in combat, and you don't want to interfere.");
-                                    SendPrompt();
-                                }
-                            } else {
-                                SendMessage("You don't think that's such a great idea.");
-                                SendPrompt();
-                            }
-                        } else {
-                            SendMessage("Who are you trying to kill, exactly? There's nobody named {0} here.", name);
-                            SendPrompt();
-                        }
-                    }
-                    return null;
-                case "help":
-                    SendMessage("You can <say> things to those nearby, if you feel like chatting.");
-                    SendMessage("You can also <tell> somebody things if you wish to speak privately.");
-                    SendMessage("You can also <emote> within sight of others.");
-                    SendMessage("If you're feeling lost, try taking a <look> around.");
-                    SendMessage("If you wish to <go> out an exit, simply speak its name or number.");
-                    SendMessage("Looking to make trouble? Try to <kill> someone!");
-                    SendPrompt();
-                    return null;
-                default:
-                    SendMessage("Hmm... that doesn't make any sense. Do you need some <help>?");
-                    SendPrompt();
-                    return null;
+            var cmd = _Commands.FindByKeyStart(firstWord);
+            if (cmd == null) {
+                SendMessage("Hmm... that doesn't make any sense. Do you need some <help>?");
+                SendPrompt();
+                return null;
             }
+            return cmd.Value.Invoke(this, words);
         }
 
         private IEnumerator<object> PromptHelper (IEnumerator<object> task, OnComplete onComplete) {
