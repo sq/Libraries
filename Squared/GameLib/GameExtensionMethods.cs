83912515f5f5ac9ab249511c557bf496da7e4eff
kevin.gadd <kevin.gadd@1953d6fe-3836-0410-a91e-07494a8b0067>
2008-12-21 12:52:04 +0000

    Add Random.NextFloat(min, max) and Random.NextDouble(min, max)
    git-svn-id: http://fracture.googlecode.com/svn/trunk@210 1953d6fe-3836-0410-a91e-07494a8b0067

---
+++ Squared/GameLib/GameExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Squared.Game {
+    public static class GameExtensionMethods {
+        public static double NextDouble (this Random random, double min, double max) {
+            return (random.NextDouble() * (max - min)) + min;
+        }
+
+        public static float NextFloat (this Random random, float min, float max) {
+            return ((float)random.NextDouble() * (max - min)) + min;
+        }
+    }
+}
