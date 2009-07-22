c351dafd20bbed3e4fe766867b4be89f169cae89
kevin.gadd <kevin.gadd@1953d6fe-3836-0410-a91e-07494a8b0067>
2009-07-22 11:29:24 +0000

    Add the Squared.Util.Event.EventBus class and associated classes/structures, along with some automated tests
    git-svn-id: http://fracture.googlecode.com/svn/trunk@283 1953d6fe-3836-0410-a91e-07494a8b0067

---
+++ Squared/Util/EventBus.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Squared.Util.Event {
+    public struct EventFilter {
+        public readonly object Source;
+        public readonly string Type;
+
+        public EventFilter (object source, string type) {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            if (type == null)
+                throw new ArgumentNullException("type");
+
+            Source = source;
+            Type = type;
+        }
+
+        public override int GetHashCode () {
+            return Source.GetHashCode() ^ Type.GetHashCode();
+        }
+    }
+
+    public class EventFilterComparer : IEqualityComparer<EventFilter> {
+        public bool Equals (EventFilter x, EventFilter y) {
+            return (x.Source == y.Source) && (x.Type == y.Type);
+        }
+
+        public int GetHashCode (EventFilter obj) {
+            return obj.GetHashCode();
+        }
+    }
+
+    public class EventInfo {
+        public readonly object Source;
+        public readonly string Type;
+        public readonly object Arguments;
+
+        private bool _IsConsumed;
+
+        public bool IsConsumed {
+            get {
+                return _IsConsumed;
+            }
+        }
+
+        public void Consume () {
+            _IsConsumed = true;
+        }
+
+        public EventInfo (object source, string type, object arguments) {
+            Source = source;
+            Type = type;
+            Arguments = arguments;
+            _IsConsumed = false;
+        }
+    }
+
+    public delegate void EventSubscriber (EventInfo e);
+    public delegate void TypedEventSubscriber<T> (EventInfo e, T arguments) where T : class;
+
+    public class EventSubscriberList : List<EventSubscriber> {
+    }
+
+    public struct EventSubscription : IDisposable {
+        public readonly EventBus EventBus;
+        public readonly EventSubscriber EventSubscriber;
+
+        private EventFilter _EventFilter;
+
+        public EventFilter EventFilter {
+            get {
+                return _EventFilter;
+            }
+        }
+
+        public EventSubscription (EventBus eventBus, ref EventFilter eventFilter, EventSubscriber subscriber) {
+            EventBus = eventBus;
+            _EventFilter = eventFilter;
+            EventSubscriber = subscriber;
+        }
+
+        public void Dispose () {
+            EventBus.Unsubscribe(ref _EventFilter, EventSubscriber);
+        }
+    }
+
+    public struct EventThunk {
+        public readonly EventBus EventBus;
+        public readonly object Sender;
+        public readonly string Type;
+
+        public EventThunk (EventBus eventBus, object sender, string type) {
+            EventBus = eventBus;
+            Sender = sender;
+            Type = type;
+        }
+
+        public void Broadcast (object arguments) {
+            EventBus.Broadcast(Sender, Type, arguments);
+        }
+
+        public void Broadcast () {
+            EventBus.Broadcast(Sender, Type, null);
+        }
+    }
+
+    public class EventBus : IDisposable {
+        public static readonly object AnySource = "<Any Source>";
+        public static readonly string AnyType = "<Any Type>";
+
+        private Dictionary<EventFilter, EventSubscriberList> _Subscribers =
+            new Dictionary<EventFilter, EventSubscriberList>(new EventFilterComparer());
+
+        public EventBus () {
+        }
+
+        private void CreateFilter (object source, string type, out EventFilter filter) {
+            filter = new EventFilter(source ?? AnySource, type ?? AnyType);
+        }
+
+        public EventSubscription Subscribe (object source, string type, EventSubscriber subscriber) {
+            EventFilter filter;
+            CreateFilter(source, type, out filter);
+
+            EventSubscriberList subscribers;
+            if (!_Subscribers.TryGetValue(filter, out subscribers)) {
+                subscribers = new EventSubscriberList();
+                _Subscribers[filter] = subscribers;
+            }
+
+            subscribers.Add(subscriber);
+
+            return new EventSubscription(this, ref filter, subscriber);
+        }
+
+        public EventSubscription Subscribe<T> (object source, string type, TypedEventSubscriber<T> subscriber) 
+            where T : class {
+            return Subscribe(source, type, (e) => subscriber(e, e.Arguments as T));
+        }
+
+        public bool Unsubscribe (object source, string type, EventSubscriber subscriber) {
+            EventFilter filter;
+            CreateFilter(source, type, out filter);
+            return Unsubscribe(ref filter, subscriber);
+        }
+
+        public bool Unsubscribe (ref EventFilter filter, EventSubscriber subscriber) {
+            EventSubscriberList subscribers;
+            if (_Subscribers.TryGetValue(filter, out subscribers))
+                return subscribers.Remove(subscriber);
+
+            return false;
+        }
+
+        public void Broadcast (object source, string type, object arguments) {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            if (type == null)
+                throw new ArgumentNullException("type");
+
+            EventInfo info = null;
+            EventSubscriberList subscribers;
+            EventFilter filter;
+
+            for (int i = 0; i < 4; i++) {
+                CreateFilter(                    
+                    (i & 1) == 1 ? source : AnySource,
+                    (i & 2) == 2 ? type : AnyType,
+                    out filter
+                );
+
+                if (!_Subscribers.TryGetValue(filter, out subscribers))
+                    continue;
+
+                int count = subscribers.Count;
+                if (count <= 0)
+                    continue;
+
+                if (info == null)
+                    info = new EventInfo(source, type, arguments);
+
+                using (var b = BufferPool<EventSubscriber>.Allocate(count)) {
+                    var temp = b.Data;
+                    subscribers.CopyTo(0, temp, 0, count);
+
+                    for (int j = count - 1; j >= 0; j--) {
+                        var subscriber = temp[j];
+                        subscriber(info);
+
+                        if (info.IsConsumed)
+                            return;
+                    }
+                }
+            }
+        }
+
+        public EventThunk GetThunk (object sender, string type) {
+            return new EventThunk(this, sender, type);
+        }
+
+        public void Dispose () {
+            _Subscribers.Clear();
+        }
+    }
+}
---
+++ Squared/Util/UtilTests/Tests/EventTests.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Squared.Util.Event {
+    [TestFixture]
+    public class EventTests {
+        EventBus Bus;
+
+        [SetUp]
+        public void SetUp () {
+            Bus = new EventBus();
+        }
+
+        [Test]
+        public void TestSubscribePrecise () {
+            var trace = new List<string>();
+            var sender = "Foo";
+
+            Bus.Subscribe(sender, "Test", (e) => trace.Add(e.Type));
+
+            Bus.Broadcast(sender, "Test", null);
+            Assert.AreEqual(new string[] { "Test" }, trace.ToArray());
+
+            Bus.Broadcast(sender, "Baz", null);
+            Bus.Broadcast("Bar", "Test", null);
+            Bus.Broadcast("Bar", "Baz", null);
+            Assert.AreEqual(new string[] { "Test" }, trace.ToArray());
+        }
+
+        [Test]
+        public void TestSubscribeGeneral () {
+            var trace = new List<string>();
+            var sender = "Foo";
+
+            Bus.Subscribe(null, "Test", (e) => trace.Add(String.Format("{0}.{1}", e.Source, e.Type)));
+            Bus.Subscribe(sender, null, (e) => trace.Add(String.Format("{0}.{1}", e.Source, e.Type)));
+
+            Bus.Broadcast(sender, "Test", null);
+            Bus.Broadcast(sender, "Baz", null);
+            Bus.Broadcast("Bar", "Test", null);
+            Bus.Broadcast("Bar", "Baz", null);
+
+            Assert.AreEqual(new string[] { 
+                "Foo.Test",
+                "Foo.Test", // Event wasn't consumed so we get it twice
+                "Foo.Baz",
+                "Bar.Test",
+                }, trace.ToArray()
+            );
+        }
+
+        [Test]
+        public void TestConsumePrecise () {
+            var trace = new List<string>();
+            var sender = "Foo";
+
+            Bus.Subscribe(sender, "Test", (e) => {
+                trace.Add(String.Format("a:{0}", e.Type));
+            });
+            Bus.Subscribe(sender, "Test", (e) => {
+                trace.Add(String.Format("b:{0}", e.Type));
+                e.Consume();
+            });
+            Bus.Subscribe(sender, "Test", (e) => {
+                trace.Add(String.Format("c:{0}", e.Type));
+            });
+
+            Bus.Broadcast(sender, "Test", null);
+
+            Assert.AreEqual(new string[] { 
+                "c:Test",
+                "b:Test"
+                }, trace.ToArray()
+            );
+        }
+
+        [Test]
+        public void TestConsumeGeneral () {
+            var trace = new List<string>();
+            var sender = "Foo";
+
+            Bus.Subscribe(sender, "Test", (e) => {
+                trace.Add(String.Format("a:{0}", e.Type));
+            });
+            Bus.Subscribe(sender, "Test", (e) => {
+                trace.Add(String.Format("b:{0}", e.Type));
+                e.Consume();
+            });
+
+            Bus.Subscribe(null, "Test", (e) => {
+                trace.Add(String.Format("c:{0}", e.Type));
+                e.Consume();
+            });
+            Bus.Subscribe(null, "Test", (e) => {
+                trace.Add(String.Format("d:{0}", e.Type));
+            });
+
+            Bus.Broadcast(sender, "Test", null);
+
+            Assert.AreEqual(new string[] { 
+                "d:Test",
+                "c:Test"
+                }, trace.ToArray()
+            );
+        }
+
+        [Test]
+        public void TestUnsubscribe () {
+            var trace = new List<string>();
+            var sender = "Foo";
+
+            var handlers = new EventSubscriber[] {
+                (e) => trace.Add("a"),
+                (e) => trace.Add("b"),
+                (e) => trace.Add("c")
+            };
+
+            foreach (var handler in handlers)
+                Bus.Subscribe(sender, "Test", handler);
+
+            Bus.Unsubscribe(sender, "Test", handlers[1]);
+
+            Bus.Broadcast(sender, "Test", null);
+            Assert.AreEqual(new string[] { "c", "a" }, trace.ToArray());
+        }
+
+        [Test]
+        public void TestDisposeSubscription () {
+            var trace = new List<string>();
+            var sender = "Foo";
+
+            var handlers = new EventSubscriber[] {
+                (e) => trace.Add("a"),
+                (e) => trace.Add("b"),
+                (e) => trace.Add("c")
+            };
+
+            var subscriptions = (from handler in handlers select Bus.Subscribe(sender, "Test", handler)).ToArray();
+
+            subscriptions[1].Dispose();
+
+            Bus.Broadcast(sender, "Test", null);
+            Assert.AreEqual(new string[] { "c", "a" }, trace.ToArray());
+        }
+
+        [Test]
+        public void TestEventThunk () {
+            var trace = new List<string>();
+            var sender = "Foo";
+
+            Bus.Subscribe(sender, "Test", (e) => trace.Add(e.Type));
+
+            var thunk = Bus.GetThunk(sender, "Test");
+            thunk.Broadcast();
+
+            Assert.AreEqual(new string[] { "Test" }, trace.ToArray());
+        }
+
+        [Test]
+        public void TestTypedSubscription () {
+            var trace = new List<string>();
+            var sender = "Foo";
+
+            Bus.Subscribe<string>(sender, "Test", (e, text) => trace.Add(text));
+
+            Bus.Broadcast(sender, "Test", "pancakes");
+            Bus.Broadcast(sender, "Test", 5);
+
+            Assert.AreEqual(new string[] { "pancakes", null }, trace.ToArray());
+        }
+
+        [TearDown]
+        public void TearDown () {
+            Bus.Dispose();
+            Bus = null;
+        }
+    }
+}
