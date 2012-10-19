e442eb186a3f60af95c2f8d5286f056821b11fce
Kevin Gadd <kevin.gadd@gmail.com>
2012-10-19 04:27:25 -0700

    Add a Timsort implementation to Squared.Util.

---
+++ Squared/Util/UtilTests/Tests/SortTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Squared.Util {
+    [TestFixture]
+    public class SortTests {
+        public readonly int[] SortedSequence, ReversedSequence, RandomSequence;
+
+        public SortTests () {
+            SortedSequence = new[] { 1, 2, 4, 8, 16, 32, 64, 128, 129, 130, 140, 150, 160, 1234567 };
+            ReversedSequence = SortedSequence.Reverse().ToArray();
+
+            // Fixed seed for reproducability
+            var rng = new Random(123456);
+            RandomSequence = new int[4096];
+
+            for (var i = 0; i < RandomSequence.Length; i++)
+                RandomSequence[i] = rng.Next();
+        }
+
+        private void AssertSortsCorrectly<T> (T[] theSequence, IComparer<T> comparer = null) {
+            var BCLCopy = (T[])theSequence.Clone();
+            var TimsortCopy = (T[])theSequence.Clone();
+
+            Array.Sort(BCLCopy, comparer ?? Comparer<T>.Default);
+            Sort.Timsort(TimsortCopy, comparer: comparer);
+
+            Assert.AreEqual(BCLCopy, TimsortCopy, "Sorted sequences are not equal");
+        }
+
+        [Test]
+        public void SortsSortedSequence () {
+            AssertSortsCorrectly(SortedSequence);
+        }
+
+        [Test]
+        public void SortsReversedSequence () {
+            AssertSortsCorrectly(ReversedSequence);
+        }
+
+        [Test]
+        public void SortsRandomSequence () {
+            AssertSortsCorrectly(RandomSequence);
+        }
+    }
+}
