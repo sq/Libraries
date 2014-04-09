5c3695687d6f386ca4e78a5e0dbfffdaf9d50f01
Kevin Gadd <kevin.gadd@gmail.com>
2014-04-08 18:48:10 -0700

    Checkpoint new task-based HttpListener replacement, HttpServer

---
+++ Squared/TaskLib/Http/HttpServer.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Squared.Task.Http {
+    public class HttpServer : IDisposable {
+        public class EndPointList : IEnumerable<EndPoint> {
+            public readonly HttpServer Owner;
+            private readonly HashSet<EndPoint> EndPoints = new HashSet<EndPoint>();  
+
+            internal EndPointList (HttpServer owner) {
+                Owner = owner;
+            }
+
+            private void CheckInvariant () {
+                if ((Owner == null) || Owner.IsDisposed)
+                    throw new ObjectDisposedException("Owner");
+
+                if (Owner.IsListening)
+                    throw new InvalidOperationException("Endpoint list may not be modified while server is listening");
+            }
+
+            public void Add (EndPoint endPoint) {
+                CheckInvariant();
+
+                EndPoints.Add(endPoint);
+            }
+
+            public void Add (params EndPoint[] endPoints) {
+                CheckInvariant();
+
+                foreach (var ep in endPoints)
+                    EndPoints.Add(ep);
+            }
+
+            public bool Remove (EndPoint endPoint) {
+                CheckInvariant();
+
+                return EndPoints.Remove(endPoint);
+            }
+
+            public EndPoint[] ToArray () {
+                return EndPoints.ToArray();
+            }
+
+            public int Count {
+                get {
+                    return EndPoints.Count;
+                }
+            }
+
+            public IEnumerator<EndPoint> GetEnumerator () {
+                return EndPoints.GetEnumerator();
+            }
+
+            System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator () {
+                return EndPoints.GetEnumerator();
+            }
+        }
+
+        private class ListenerContext : IDisposable {
+            public readonly EndPoint[] EndPoints;
+            public readonly Socket[] Sockets;
+            public readonly SignalFuture Started = new SignalFuture();
+
+            public ListenerContext (IEnumerable<EndPoint> endPoints) {
+                EndPoints = endPoints.ToArray();
+                Sockets = new Socket[EndPoints.Length];
+            }
+
+            public void BindAll () {
+                for (var i = 0; i < EndPoints.Length; i++) {
+                    var endPoint = EndPoints[i];
+                    var socket = Sockets[i] = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.IP);
+                    socket.Bind(endPoint);
+                }
+            }
+
+            public void Dispose () {
+                for (var i = 0; i < Sockets.Length; i++) {
+                    if (Sockets[i] != null)
+                        Sockets[i].Dispose();
+
+                    Sockets[i] = null;
+                }
+
+                Started.Dispose();
+            }
+        }
+
+        public readonly TaskScheduler Scheduler;
+        public readonly EndPointList EndPoints;
+
+        public BackgroundTaskErrorHandler ListenerErrorHandler;
+
+        private IFuture _ActiveListener = null;
+
+        public HttpServer (TaskScheduler scheduler) {
+            EndPoints = new EndPointList(this);
+
+            Scheduler = scheduler;
+        }
+
+        public bool IsDisposed {
+            get;
+            private set;
+        }
+
+        public bool IsListening {
+            get {
+                return (_ActiveListener != null) && !_ActiveListener.Completed;
+            }
+        }
+
+        public SignalFuture StartListening () {
+            if (IsListening)
+                throw new InvalidOperationException("Already listening");
+
+            var context = new ListenerContext(EndPoints.ToArray());
+            _ActiveListener = Scheduler.Start(ListenerTask(context));
+
+            return context.Started;
+        }
+
+        private IEnumerator<object> ListenerTask (ListenerContext context) {
+            using (context) {
+                yield return Future.RunInThread(context.BindAll);
+
+                context.Started.Complete();
+
+                var wfns = new WaitForNextStep();
+
+                while (true) {
+                    yield return wfns;
+                }
+            }
+        }
+
+        public void StopListening () {
+            if (_ActiveListener != null) {
+                _ActiveListener.Dispose();
+                _ActiveListener = null;
+            }
+        }
+
+        public void Dispose () {
+            if (IsDisposed)
+                return;
+
+            IsDisposed = true;
+
+            if (IsListening)
+                StopListening();
+        }
+    }
+}
---
+++ Squared/TaskLib/TaskLibTests/Tests/HttpTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using System.IO;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+using System.Linq;
+
+namespace Squared.Task.Http {
+    [TestFixture]
+    public class HttpTests {
+        static readonly IPEndPoint TestPort1 = new IPEndPoint(IPAddress.Any, 12345);
+        static readonly IPEndPoint TestPort2 = new IPEndPoint(IPAddress.Any, 12346);
+
+        public TaskScheduler Scheduler;
+        public HttpServer Server;
+
+        [SetUp]
+        public void SetUp () {
+            Scheduler = new TaskScheduler();
+            Server = new HttpServer(Scheduler);
+        }
+
+        private static bool IsEndPointBound (EndPoint endPoint) {
+            try {
+                using (var socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.IP))
+                    socket.Bind(endPoint);
+
+                return false;
+            } catch (SocketException se) {
+                if (se.ErrorCode == 10048)
+                    return true;
+
+                throw;
+            }
+        }
+
+        [Test]
+        public void ServerBindsPortsWhileListeningAndUnbindsWhenStopped () {
+            Assert.IsFalse(Server.IsListening);
+
+            Server.EndPoints.Add(TestPort1);
+            Server.EndPoints.Add(TestPort2);
+            Scheduler.WaitFor(Server.StartListening());
+
+            Assert.IsTrue(Server.IsListening);
+
+            Assert.IsTrue(IsEndPointBound(TestPort1));
+            Assert.IsTrue(IsEndPointBound(TestPort2));
+
+            Server.StopListening();
+
+            Assert.IsFalse(IsEndPointBound(TestPort1));
+            Assert.IsFalse(IsEndPointBound(TestPort2));
+        }
+
+        [Test]
+        public void ThrowsIfYouAddOrRemoveEndPointsWhileListening () {
+            Scheduler.WaitFor(Server.StartListening());
+
+            Assert.Throws<InvalidOperationException>(
+                () => Server.EndPoints.Add(TestPort1)
+            );
+            Assert.Throws<InvalidOperationException>(
+                () => Server.EndPoints.Remove(TestPort1)
+            );
+        }
+
+        [TearDown]
+        public void TearDown () {
+            if (Server != null)
+                Server.Dispose();
+
+            if (Scheduler != null)
+                Scheduler.Dispose();
+
+            Server = null;
+            Scheduler = null;
+        }
+    }
+}
