71548e29b5370675668b715f32e847ac147157ad
K. Gadd <kg@luminance.org>
2014-05-01 07:11:24 -0700

    Add Vector2.Floor

--- Squared/GameLib/GameExtensionMethods.cs
+++ Squared/GameLib/GameExtensionMethods.cs
@@ -33,6 +33,10 @@ namespace Squared.Game {
             return new Vector2(vector.Y, -vector.X);
         }
 
+        public static Vector2 Floor (this Vector2 vector) {
+            return new Vector2((float)Math.Floor(vector.X), (float)Math.Floor(vector.Y));
+        }
+
         public static Vector2 Round (this Vector2 vector) {
             return new Vector2((float)Math.Round(vector.X), (float)Math.Round(vector.Y));
         }
