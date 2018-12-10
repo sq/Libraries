cea3906cd6ba23b3d06491d9f680860cea7ac286
Katelyn Gadd <kg@luminance.org>
2018-12-09 23:52:32 -0800

    Add native library for STB_Image / STB_Image_Write Checkpoint managed STB_Image / STB_Image_Write library Update ignore list

---
+++ Squared/Render.STB/Native.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Squared.Render.STB.Native {
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public unsafe delegate int ReadCallback (void* userData, byte* data, int size);
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public unsafe delegate void SkipCallback (void* userData, int count);
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public unsafe delegate int EOFCallback (void* userData);
+
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct STBI_IO_Callbacks {
+        [MarshalAs(UnmanagedType.FunctionPtr)]
+        public ReadCallback read;
+        [MarshalAs(UnmanagedType.FunctionPtr)]
+        public SkipCallback skip;
+        [MarshalAs(UnmanagedType.FunctionPtr)]
+        public EOFCallback eof;
+    }
+
+    public static unsafe partial class API {
+#if DEBUG
+        const string DllName = "stbi-x86-Debug";
+#else
+        const string DllName = "stbi-x86-Release";
+#endif
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, SetLastError = false)]
+        public static extern void stbi_image_free (void* ptr);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, SetLastError = false)]
+        public static extern byte* stbi_load_from_memory (byte* buffer, int len, out int x, out int y, out int channels, int desired_channels);
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, SetLastError = false)]
+        public static extern byte* stbi_load_from_callbacks (ref STBI_IO_Callbacks clbk, void *user, out int x, out int y, out int channels, int desired_channels);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, SetLastError = false)]
+        public static extern ushort* stbi_load_16_from_memory (byte* buffer, int len, out int x, out int y, out int channels, int desired_channels);
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, SetLastError = false)]
+        public static extern ushort* stbi_load_16_from_callbacks (ref STBI_IO_Callbacks clbk, void *user, out int x, out int y, out int channels, int desired_channels);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, SetLastError = false)]
+        public static extern int stbi_info_from_memory (byte* buffer, int len, out int x, out int y, out int comp);
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, SetLastError = false)]
+        public static extern int stbi_info_from_callbacks (ref STBI_IO_Callbacks clbk, void *user, out int x, out int y, out int comp);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, SetLastError = false)]
+        public static extern int stbi_is_16_bit_from_memory (byte* buffer, int len);
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, SetLastError = false)]
+        public static extern int stbi_is_16_bit_from_callbacks (ref STBI_IO_Callbacks clbk, void *user);
+    }
+}
---
+++ Squared/Render.STB/STBI.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace Squared.Render.STB {
+    public unsafe class Image : IDisposable {
+        public void* Handle { get; private set; }
+
+        public void Dispose () {
+            if (Handle != null)
+
+            throw new NotImplementedException();
+        }
+    }
+
+    public enum ImagePrecision {
+        Default = 0,
+        Byte = 8,
+        UInt16 = 16
+    }
+
+    public enum ImageChannels {
+        Default = 0,
+        Grey = 1,
+        GreyAlpha = 2,
+        RGB = 3,
+        RGBA = 4
+    }
+}
