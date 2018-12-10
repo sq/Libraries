8823b05e7765485fff9fab79faebbf3721ebd585
Katelyn Gadd <kg@luminance.org>
2018-12-09 23:32:33 -0800

    Add project to generate a DLL for stb_image and stb_image_write Update gitignore list

---
+++ Squared/STBNative/main.cpp
@@ -0,0 +1,22 @@
+#define STBIDEF  extern "C" __declspec(dllexport)
+#define STBIWDEF extern "C" __declspec(dllexport)
+
+#define STBI_NO_STDIO
+#define STBI_WRITE_NO_STDIO
+
+#define STBI_WRITE_NO_HDR
+
+#define STBI_NO_BMP
+#define STBI_NO_GIF
+#define STBI_NO_HDR
+#define STBI_NO_PIC
+#define STBI_NO_PNM
+
+#include "stb_image.h"
+#include "stb_image_write.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+
+#include "stb_image.h"
+#include "stb_image_write.h"
