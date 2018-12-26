a300637a5691203cfcdd91c9fcb895de5cef6c75
Katelyn Gadd <kg@luminance.org>
2018-12-26 03:23:43 -0800

    Remove playstation mobile cruft

--- Squared/RenderLib/PSM/generate_shaders.bat
+++
@@ -1,7 +0,0 @@
-@echo Generating shaders...
-@rmdir /s/q generated
-@mkdir generated
-@mkdir generated\shaders
-@for %%f IN (shaders\*.fcg) DO tcc.exe -E %%f -o generated\%%f
-@for %%f IN (shaders\*.vcg) DO tcc.exe -E %%f -o generated\%%f
-@echo Done.
