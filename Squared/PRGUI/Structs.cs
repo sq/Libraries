a81879dba484a2df1509d04b4858b28444af88af
Katelyn Gadd <kg@luminance.org>
2020-09-02 06:30:44 -0700

    Update dep Checkpoint

---
+++ Squared/PRGUI/Structs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Squared.PRGUI {
+    public struct ControlHeader {
+        public int ID;
+        public Type ControlType;
+        public object UserData;
+
+        public ControlHeader (int id, Type type, object userData = null) {
+            ID = id;
+            ControlType = type;
+            UserData = userData;
+        }
+    }
+}
