3cf2e9d26d00d37b098ddcaebc59ec414af97731
Katelyn Gadd <kg@luminance.org>
2020-10-03 23:19:34 -0700

    Update tween and add a few basic tests

--- Squared/Util/Tween.cs
+++ Squared/Util/Tween.cs
@@ -11,6 +11,7 @@ namespace Squared.Util {
 
         private static readonly BoundInterpolatorSource<T, Tween<T>> GetValue;
 
+        public readonly int RepeatCount;
         public readonly T From, To;
         public readonly BoundInterpolator<T, Tween<T>> Interpolator;
         public readonly long StartedWhen, EndWhen;
@@ -32,54 +33,69 @@ namespace Squared.Util {
             From = To = value;
             StartedWhen = EndWhen = 0;
             Interpolator = null;
+            RepeatCount = 0;
         }
 
         public Tween (
             T from, T to,
             long startWhen, long endWhen,
-            BoundInterpolator<T, Tween<T>> interpolator = null 
+            BoundInterpolator<T, Tween<T>> interpolator = null,
+            int repeatCount = 0
         ) {
             From = from;
             To = to;
             StartedWhen = startWhen;
             EndWhen = endWhen;
             Interpolator = interpolator ?? Interpolators<T>.GetBoundDefault<Tween<T>>();
+            RepeatCount = repeatCount;
         }
 
         public static Tween<T> StartNow (
             T from, T to, long ticks, long? delay = null,
-            long? now = null, BoundInterpolator<T, Tween<T>> interpolator = null
+            long? now = null, BoundInterpolator<T, Tween<T>> interpolator = null, int repeatCount = 0
         ) {
             var _now = now.HasValue ? now.Value : Time.Ticks;
             return new Tween<T>(
                 from, to,
                 _now + delay.GetValueOrDefault(0),
                 _now + delay.GetValueOrDefault(0) + ticks,
-                interpolator
+                interpolator: interpolator, repeatCount: repeatCount
             );
         }
 
         public static Tween<T> StartNow (
             T from, T to, float seconds, float? delay = null,
-            long? now = null, BoundInterpolator<T, Tween<T>> interpolator = null
+            long? now = null, BoundInterpolator<T, Tween<T>> interpolator = null, int repeatCount = 0
         ) {
             return StartNow(
                 from, to,
                 ticks: TimeSpan.FromSeconds(seconds).Ticks,
                 delay: TimeSpan.FromSeconds(delay.GetValueOrDefault(0)).Ticks,
-                now: now, interpolator: interpolator
+                now: now, interpolator: interpolator, repeatCount: repeatCount
             );
         }
 
-        public float GetProgress (long now) {
-            if ((EndWhen <= StartedWhen) || (EndWhen <= 0)) {
-                if (now < StartedWhen)
-                    return 0f;
-                else
-                    return 1f;
-            }
+        private float GetProgressUnclamped (long now) {
+            if (now < StartedWhen)
+                return 0f;
 
-            return Arithmetic.Clamp((float)((now - StartedWhen) / (double)(EndWhen - StartedWhen)), 0, 1);
+            if ((EndWhen <= StartedWhen) || (EndWhen <= 0))
+                return 1f;
+
+            var durationTicks = EndWhen - StartedWhen;
+            var elapsedTicks = now - StartedWhen;
+
+            return (float)(elapsedTicks / ((double)durationTicks));
+        }
+
+        public float GetProgress (long now) {
+            var unclamped = GetProgressUnclamped(now);
+            if (RepeatCount == int.MaxValue)
+                return Math.Max(0, unclamped);
+
+            var clampedToRepeatCount = Arithmetic.Clamp(unclamped, 0, 1f + RepeatCount);
+            var wrapped = Arithmetic.WrapInclusive(clampedToRepeatCount, 0, 1f);
+            return wrapped;
         }
 
         public T Get (float now) {
@@ -109,7 +125,10 @@ namespace Squared.Util {
             if (EndWhen <= 0)
                 return false;
 
-            return GetProgress(now) >= 1;
+            if (RepeatCount == int.MaxValue)
+                return false;
+
+            return GetProgressUnclamped(now) >= (1f + RepeatCount);
         }
 
         public static implicit operator Tween<T> (T value) {
---
+++ Squared/Util/UtilTests/Tests/TweenTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Squared.Util;
+
+namespace Squared.Util {
+    [TestFixture]
+    public class TweenTests {
+        [Test]
+        public void Constant () {
+            var t0 = (Tween<double>)0;
+            var t1 = (Tween<double>)1;
+            Assert.AreEqual(0, t0.Get(0));
+            Assert.AreEqual(0, t0.Get(1));
+            Assert.AreEqual(0, t0.Get(long.MaxValue));
+            Assert.AreEqual(0, t0.Get(long.MinValue));
+            Assert.AreEqual(1, t1.Get(0));
+            Assert.AreEqual(1, t1.Get(1));
+            Assert.AreEqual(1, t1.Get(long.MaxValue));
+            Assert.AreEqual(1, t1.Get(long.MinValue));
+        }
+
+        [Test]
+        public void OneShot () {
+            var t = Tween<double>.StartNow(0, 2, 1024L, 256L, 0L);
+            Assert.AreEqual(0, t.Get(long.MinValue));
+            Assert.AreEqual(0, t.Get(-256L));
+            Assert.AreEqual(0, t.Get(0L));
+            Assert.AreEqual(0, t.Get(256L));
+            Assert.AreEqual(2, t.Get(1024L + 256L));
+            Assert.AreEqual(2, t.Get(4096L));
+            Assert.AreEqual(2, t.Get(long.MaxValue));
+        }
+    }
+}
