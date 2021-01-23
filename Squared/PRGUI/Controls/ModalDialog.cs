4489ac3a8df2f76e7332ae3dca072f8684e3fb6f
Katelyn Gadd <kg@luminance.org>
2021-01-23 12:03:31 -0800

    Checkpoint

--- Squared/PRGUI/Controls/ModalDialog.cs
+++ Squared/PRGUI/Controls/ModalDialog.cs
@@ -6,20 +6,65 @@ using System.Threading.Tasks;
 using Microsoft.Xna.Framework.Input;
 using Squared.PRGUI.Accessibility;
 using Squared.PRGUI.Decorations;
+using Squared.PRGUI.Imperative;
 using Squared.PRGUI.Layout;
 using Squared.Render.Convenience;
 using Squared.Threading;
 using Squared.Util;
+using Squared.Util.Event;
 
 namespace Squared.PRGUI.Controls {
-    public class ModalDialog : Window, IModal, IAcceleratorSource {
+    public delegate void TypedContainerContentsDelegate<TParameters> (ref ContainerBuilder builder, TParameters parameters);
+
+    public class ModalDialog<TParameters, TResult> : Window, IModal, IAcceleratorSource {
         public event Action<IModal> Shown, Closed;
+        public event Func<IModal, bool> Accepted, Cancelled;
+
+        public TParameters Parameters;
 
         protected Control _FocusDonor;
         public Control FocusDonor => _FocusDonor;
 
-        public Control AcceptControl, CancelControl;
-        public object CancelResult;
+        private bool IsAcceptHandlerRegistered, IsCancelHandlerRegistered;
+        private Control _AcceptControl, _CancelControl;
+
+        public Control AcceptControl {
+            get => _AcceptControl;
+            set {
+                if (_AcceptControl == value)
+                    return;
+                _AcceptControl = value;
+                RegisterHandler(_AcceptControl, OnAcceptClick, ref IsAcceptHandlerRegistered);
+            }
+        }
+
+        public Control CancelControl {
+            get => _CancelControl;
+            set {
+                if (_CancelControl == value)
+                    return;
+                _CancelControl = value;
+                RegisterHandler(_CancelControl, OnCancelClick, ref IsCancelHandlerRegistered);
+            }
+        }
+
+        private TResult _AcceptResult, _CancelResult;
+        // private bool HasAcceptResult, HasCancelResult;
+
+        public TResult AcceptResult {
+            get => _AcceptResult;
+            set {
+                _AcceptResult = value;
+                // HasAcceptResult = true;
+            }
+        }
+        public TResult CancelResult {
+            get => _CancelResult;
+            set {
+                _CancelResult = value;
+                // HasCancelResult = true;
+            }
+        }
 
         // FIXME: Customizable?
         bool IModal.FadeBackground => true;
@@ -30,16 +75,46 @@ namespace Squared.PRGUI.Controls {
         public bool IsActive { get; private set; }
         private bool IsFadingOut;
 
-        private Future<object> NextResultFuture = null;
+        private Future<TResult> NextResultFuture = null;
 
-        public ModalDialog ()
+        public ModalDialog (TParameters parameters)
             : base () {
+            Parameters = parameters;
             Appearance.Opacity = 0f;
         }
 
-        public static Future<object> ShowNew (UIContext context) {
-            var modal = new ModalDialog {
-            };
+        private void RegisterHandler (Control target, EventSubscriber handler, ref bool isRegistered) {
+            if (Context == null)
+                return;
+            if (target == null)
+                return;
+
+            Context.EventBus.Subscribe(target, UIEvents.Click, handler);
+            isRegistered = true;
+        }
+
+        protected virtual void OnAcceptClick (IEventInfo e) {
+            if ((Accepted != null) && Accepted(this)) {
+                e.Consume();
+                return;
+            }
+
+            e.Consume();
+            Close(AcceptResult);
+        }
+
+        protected virtual void OnCancelClick (IEventInfo e) {
+            if ((Cancelled != null) && Cancelled(this)) {
+                e.Consume();
+                return;
+            }
+
+            e.Consume();
+            Close(CancelResult);
+        }
+
+        public static Future<TResult> ShowNew (UIContext context, TParameters parameters) {
+            var modal = new ModalDialog<TParameters, TResult>(parameters);
             return modal.Show(context);
         }
 
@@ -47,7 +122,7 @@ namespace Squared.PRGUI.Controls {
             this.Show(context);
         }
 
-        public Future<object> Show (UIContext context, Control focusDonor = null) {
+        public Future<TResult> Show (UIContext context, Control focusDonor = null) {
             SetContext(context);
             // HACK: Prevent the layout info from computing our size from being used to render us next frame
             InvalidateLayout();
@@ -58,7 +133,7 @@ namespace Squared.PRGUI.Controls {
             Intangible = false;
             if (IsActive)
                 return NextResultFuture;
-            var f = NextResultFuture = new Future<object>();
+            var f = NextResultFuture = new Future<TResult>();
             var fadeIn = context.Animations?.ShowModalDialog;
             if (fadeIn != null)
                 StartAnimation(fadeIn);
@@ -67,6 +142,11 @@ namespace Squared.PRGUI.Controls {
             GenerateDynamicContent(true);
             _FocusDonor = focusDonor ?? context.TopLevelFocused;
             context.ShowModal(this);
+            // HACK: Ensure event handlers are registered if they weren't already
+            if (!IsAcceptHandlerRegistered)
+                RegisterHandler(AcceptControl, OnAcceptClick, ref IsAcceptHandlerRegistered);
+            if (!IsCancelHandlerRegistered)
+                RegisterHandler(CancelControl, OnCancelClick, ref IsCancelHandlerRegistered);
             IsActive = true;
             if (Shown != null)
                 Shown(this);
@@ -74,10 +154,10 @@ namespace Squared.PRGUI.Controls {
         }
 
         void IModal.Close () {
-            this.Close(null);
+            this.Close(default(TResult));
         }
 
-        public void Close (object result = null) {
+        public void Close (TResult result) {
             if (!IsActive) {
                 // FIXME
                 /*
@@ -117,15 +197,20 @@ namespace Squared.PRGUI.Controls {
             if (name != UIEvents.KeyUp)
                 return false;
 
-            if ((args.Key == Keys.Enter) && (AcceptControl != null)) {
-                return Context.FireSyntheticClick(AcceptControl);
-            } else if (args.Key == Keys.Escape) {
-                if (CancelControl != null)
-                    return Context.FireSyntheticClick(CancelControl);
-                else if (CancelResult != null) {
-                    Close(CancelResult);
+            // HACK
+            var ei = new EventInfo<object>(null, this, default(EventCategoryToken), null, null, args);
+            if (args.Key == Keys.Enter) {
+                if ((AcceptControl != null) && Context.FireSyntheticClick(AcceptControl))
                     return true;
-                }
+
+                OnAcceptClick(ei);
+                return ei.IsConsumed;
+            } else if (args.Key == Keys.Escape) {
+                if ((CancelControl != null) && Context.FireSyntheticClick(CancelControl))
+                    return true;
+
+                OnCancelClick(ei);
+                return ei.IsConsumed;
             }
 
             return false;
@@ -138,6 +223,39 @@ namespace Squared.PRGUI.Controls {
                 if (CancelControl != null)
                     yield return new AcceleratorInfo(CancelControl, Keys.Escape);
             }
+        }
+    }
+
+    public class ModalDialog : ModalDialog<object, object> {
+        public ModalDialog (object parameters = null)
+            : base (parameters) {
+        }
+
+        public static ModalDialog<object, TResult> New<TResult> () {
+            return new ModalDialog<object, TResult>(null);
+        }
+
+        public static ModalDialog<TParameters, object> New<TParameters> (
+            TParameters parameters, ContainerContentsDelegate dynamicContents = null
+        ) {
+            return new ModalDialog<TParameters, object>(parameters) {
+                DynamicContents = dynamicContents
+            };
+        }
+
+        public static ModalDialog<TParameters, TResult> New<TParameters, TResult> (
+            TParameters parameters, TResult acceptResult, TResult cancelResult, 
+            ContainerContentsDelegate dynamicContents = null
+        ) {
+            return new ModalDialog<TParameters, TResult>(parameters) {
+                AcceptResult = acceptResult,
+                CancelResult = cancelResult,
+                DynamicContents = dynamicContents
+            };
+        }
+
+        public void Close () {
+            base.Close(null);
         }
     }
 }
