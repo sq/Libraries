d3691e795273092123e058a9a8de193e32a13142
Katelyn Gadd <kg@luminance.org>
2021-05-14 02:35:26 -0700

    Checkpoint

--- Squared/Threading/ThreadIdleManager.cs
+++ Squared/Threading/ThreadIdleManager.cs
@@ -7,22 +7,23 @@ using System.Threading.Tasks;
 
 namespace Squared.Threading {
     public class ThreadIdleManager : IDisposable {
-        const int State_Sleeping = 0,
-            State_WakeRequested = 1,
-            State_Running = 2,
-            State_Disposed = 3;
+        const int RunState_Sleeping = 0,
+            RunState_WakeRequested = 1,
+            RunState_Running = 2,
+            RunState_Disposed = 3;
 
-        private volatile int State = State_WakeRequested;
+        private volatile int RequestState = 0;
+        private volatile int RunState = RunState_Sleeping;
         private ManualResetEventSlim Event = new ManualResetEventSlim(true, 1);
 
         /// <returns>true if the thread is allowed to begin running, false if it has been disposed</returns>
         public bool BeginRunning () {
             // FIXME: Is this right?
             Event.Reset();
-            var previousState = Interlocked.Exchange(ref State, State_Running);
-            if (previousState == State_Disposed)
-                Volatile.Write(ref State, State_Disposed);
-            return (previousState != State_Disposed);
+            var previousState = Interlocked.Exchange(ref RunState, RunState_Running);
+            if (previousState == RunState_Disposed)
+                Volatile.Write(ref RunState, RunState_Disposed);
+            return (previousState != RunState_Disposed);
         }
 
         /// <summary>
@@ -30,17 +31,17 @@ namespace Squared.Threading {
         ///  until at least one of them runs again once
         /// </summary>
         public void Wake () {
-            var oldState = Interlocked.Exchange(ref State, State_WakeRequested);
-            if (oldState == State_Disposed) {
-                Volatile.Write(ref State, State_Disposed);
+            var wasWorkAlreadyRequested = Interlocked.Exchange(ref RequestState, 1) == 1;
+            var oldRunState = Interlocked.CompareExchange(ref RunState, RunState_WakeRequested, RunState_Sleeping);
+            var needsToSignal = oldRunState == RunState_Sleeping;
+            if (wasWorkAlreadyRequested && !needsToSignal)
                 return;
-            }
-            if (oldState == State_Sleeping) {
-                Thread.Yield();
-                // FIXME: It might be faster to check for State_Sleeping here, but I think that may be a race?
-                if (Volatile.Read(ref State) != State_Running)
-                    Event.Set();
-            }
+            if (oldRunState == RunState_Disposed)
+                return;
+
+            if (needsToSignal)
+                // FIXME: Yield here somehow
+                Event.Set();
         }
 
         /// <summary>
@@ -49,25 +50,21 @@ namespace Squared.Threading {
         /// </summary>
         /// <returns>true if a wake was requested, false if the timeout expired</returns>
         public bool Wait (int timeoutMs = -1) {
-            var oldState = Interlocked.CompareExchange(ref State, State_Sleeping, State_Running);
-            if (oldState != State_Running)
+            var oldState = Interlocked.CompareExchange(ref RunState, RunState_Sleeping, RunState_Running);
+            if (oldState != RunState_Running)
                 return true;
 
-            Thread.Yield();
-            var newState = Volatile.Read(ref State);
-            if (newState != State_Sleeping) {
-                if (newState != State_Disposed)
-                    Volatile.Write(ref State, State_Running);
+            if (Interlocked.Exchange(ref RequestState, 0) != 0)
                 return true;
-            }
 
             var result = Event.Wait(timeoutMs);
-            Interlocked.CompareExchange(ref State, State_Running, State_Sleeping);
+            Interlocked.CompareExchange(ref RunState, RunState_Running, RunState_Sleeping);
             return result;
         }
 
         public void Dispose () {
-            Volatile.Write(ref State, State_Disposed);
+            Volatile.Write(ref RunState, RunState_Disposed);
+            Event.Set();
         }
     }
 }
