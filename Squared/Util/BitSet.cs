79623434772367d9797dd7bac12eb6ec8c7e21a5
Katelyn Gadd <kg@luminance.org>
2022-07-27 03:12:38 -0700

    Add BitSet struct

---
+++ Squared/Util/BitSet.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Squared.Util.Containers {
+    public struct BitSet : IEnumerable<int> {
+        public struct IndicesEnumerator : IEnumerable<int>, IEnumerator<int> {
+            internal int Index;
+            internal UInt64 A, B;
+
+            [TargetedPatchingOptOut("")]
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            internal IndicesEnumerator (UInt64 a, UInt64 b) {
+                A = a;
+                B = b;
+                Index = -1;
+            }
+
+            public void Dispose () {
+            }
+
+            [TargetedPatchingOptOut("")]
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool MoveNext () {
+                while (++Index < Length) {
+                    ref var slot = ref FindBit(ref A, Index, out var mask);
+                    if ((slot & mask) != 0)
+                        return true;
+                }
+
+                return false;
+            }
+
+            public void Reset () {
+                Index = -1;
+            }
+
+            public IndicesEnumerator GetEnumerator () {
+                return new IndicesEnumerator(A, B);
+            }
+
+            public int Current {
+                [TargetedPatchingOptOut("")]
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+                get => Index;
+            }
+            object IEnumerator.Current => Current;
+
+            IEnumerator<int> IEnumerable<int>.GetEnumerator () => GetEnumerator();
+            IEnumerator IEnumerable.GetEnumerator () => GetEnumerator();
+        }
+
+        public const int Length = 128;
+
+        internal UInt64 A, B;
+
+        internal static void BoundsCheckFailed () {
+            throw new ArgumentOutOfRangeException("index");
+        }
+
+        [TargetedPatchingOptOut("")]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static ref UInt64 FindBit (ref UInt64 a, int index, out UInt64 mask) {
+            if ((index < 0) || (index >= Length))
+                BoundsCheckFailed();
+            int slotIndex = index / 64, localIndex = index % 64;
+            mask = 1UL << localIndex;
+            return ref Unsafe.Add(ref a, slotIndex);
+        }
+
+        public bool this [int index] {
+            [TargetedPatchingOptOut("")]
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get {
+                ref var slot = ref FindBit(ref A, index, out var mask);
+                return (slot & mask) != 0;
+            }
+            [TargetedPatchingOptOut("")]
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            set {
+                ref var slot = ref FindBit(ref A, index, out var mask);
+                if (value)
+                    slot |= mask;
+                else
+                    slot &= ~mask;
+            }
+        }
+
+        public void Add (int index) {
+            ref var slot = ref FindBit(ref A, index, out var mask);
+            slot |= mask;
+        }
+
+        public IndicesEnumerator Indices => new IndicesEnumerator(A, B);
+
+        IEnumerator<int> IEnumerable<int>.GetEnumerator () => Indices.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator () => Indices.GetEnumerator();
+
+        /// <summary>
+        /// Sets the value at an index and returns whether it was changed.
+        /// </summary>
+        /// <returns>true if the new value is different from the old value</returns>
+        public bool Change (int index, bool value) {
+            ref var slot = ref FindBit(ref A, index, out var mask);
+            if (value) {
+                if ((slot & mask) != 0)
+                    return false;
+
+                slot |= mask;
+                return true;
+            } else if ((slot & mask) == 0)
+                return false;
+
+            slot &= ~mask;
+            return true;
+        }
+
+        public void SetAll () {
+            A = B = ~0UL;
+        }
+
+        public void Clear () {
+            A = B = default;
+        }
+    }
+}
---
+++ Squared/Util/UtilTests/Tests/BitsetTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Squared.Util.Containers;
+
+namespace Squared.Util {
+    public class BitSetTests {
+        [Test]
+        public void BasicTest () {
+            var set = new BitSet();
+            Assert.IsFalse(set[0]);
+            set[0] = true;
+            set[1] = true;
+            set[69] = true;
+            set[70] = true;
+            Assert.IsTrue(set[0]);
+            Assert.IsTrue(set[1]);
+            Assert.IsTrue(set[69]);
+            Assert.IsTrue(set[70]);
+            set[1] = false;
+            set[70] = false;
+            Assert.IsTrue(set[0]);
+            Assert.IsFalse(set[1]);
+            Assert.IsTrue(set[69]);
+            Assert.IsFalse(set[70]);
+        }
+
+        [Test]
+        public void Change () {
+            var set = new BitSet();
+            Assert.IsTrue(set.Change(0, true));
+            Assert.IsFalse(set.Change(0, true));
+            Assert.IsFalse(set.Change(1, false));
+        }
+
+        [Test]
+        public void Enumerators () {
+            var set = new BitSet { 1, 3, 7, 63 };
+            set.Add(5);
+            set[7] = false;
+            Assert.AreEqual(new[] { 1, 3, 5, 63 }, set.ToArray());
+        }
+    }
+}
